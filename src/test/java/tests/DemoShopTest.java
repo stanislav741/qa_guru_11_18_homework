086120ac114fceeb05f0aaa9acc289bf2ce30e09
stanislav741 <stasdmitruk1@gmail.com>
2022-05-19 05:01:20 +0200

    Updated the scope of the test case and finished the steps

--- src/test/java/tests/DemoShopTest.java
+++ src/test/java/tests/DemoShopTest.java
@@ -1,96 +1,51 @@
 package tests;
 
 import org.openqa.selenium.Cookie;
-import io.restassured.http.Cookies;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
-
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.refresh;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.is;
 
-public class DemoShopTest {
+public class DemoShopTest extends TestBase {
 
     @Test
-    void wishlistCreateAndEmail() {
+    void wishlistCreateAndClear() {
 
-        String cookieNopCustomer = "d1d4d828-f6dc-422e-809a-c956d84f56c1";
-                when()
-                        .get("http://demowebshop.tricentis.com/")
-                .then()
-//                        .log().all()
-                        .statusCode(200);
-//                        .extract().cookie("Nop.customer");
+//Extracting an user cookie for the session
+        String cookieNopCustomer =
+            when()
+                .get("")
+            .then()
+                .statusCode(200)
+                .extract().cookie("Nop.customer");
 
-//        System.out.println(cookieNopCustomer);
-
-        open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png");
+//Applying the user cookie to the webdriver
+        open("/Themes/DefaultClean/Content/images/logo.png");
         getWebDriver().manage().addCookie(new Cookie("Nop.customer",cookieNopCustomer));
-        open("http://demowebshop.tricentis.com/");
+        open("");
 
+//Adding 5 products to the wishlist
         for(int i = 0; i<5; i++) {
-        given().
-                contentType("application/x-www-form-urlencoded; charset=UTF-8").
-             cookie("Nop.customer", cookieNopCustomer).
-             when().
-            post("http://demowebshop.tricentis.com/addproducttocart/details/43/2").
-               then().
-               log().all().
-               statusCode(200).
-               body("success", is(true));
-   }
-        refresh();
-        refresh();
-    }
-}
-//                extract().cookie("");
+            given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .cookie("Nop.customer", cookieNopCustomer)
+            .when()
+                .post("/addproducttocart/details/43/2")
+            .then()
+                .statusCode(200)
+                .body("success", is(true));
+        }
 
-////        String NopCustomer = cookieResponse.get("");
-//
-//        open("http://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png");
-//        getWebDriver().manage().addCookie(new Cookie("Nop.customer","67f44180-2aef-4cc9-ae52-f4826dc565d7"));
-////        getWebDriver().manage().addCookie(new Cookie("ARRAffinity","1818b4c81d905377ced20e7ae987703a674897394db6e97dc1316168f754a687"));
-//        open("http://demowebshop.tricentis.com/");
-//
-//    for(int i = 0; i<5; i++) {
-//        given().
-//                contentType("application/x-www-form-urlencoded; charset=UTF-8").
-////                cookies(cookieResponse).
-////        cookie("ARRAffinity", "1818b4c81d905377ced20e7ae987703a674897394db6e97dc1316168f754a687").
-//                cookie("Nop.customer", "67f44180-2aef-4cc9-ae52-f4826dc565d7").
-//                when().
-//                post("http://demowebshop.tricentis.com/addproducttocart/details/43/2").
-//                then().
-//                log().all().
-//                statusCode(200).
-//                body("success", is(true));
-//    }
-//        refresh();
-//        refresh();
-////                body("message", is("The product has been added to your " +
-////                        "\\u003ca href=\\\"/wishlist\\\"\\u003ewishlist\\u003c/a\\u003e"));
-//    }
-//
-////    @Test
-////    void setUserCookie() {
-////
-////        Map<String, String> cookieResponse =
-////        when().
-////                get("/Themes/DefaultClean/Content/images/logo.png").
-////        then().
-//////                log().all().
-////                statusCode(200).
-////                extract().cookies();
-////
-////        String cookie = cookieResponse.get("ARRAffinity");
-////
-////        System.out.println(cookie);
-////
-////    }
-//
-//
-//}
+//Opening the wishlist page and clearing all the items
+        $("#topcartlink").sibling(0).click();
+        $(".remove-from-cart").click();
+        $(byName("updatecart")).click();
+        $(byText("The wishlist is empty!")).shouldBe(visible);
+
+    }
+
+}
